5888a504cd286aebff8a2e92c879fd5e1d02b5d3
Juan Manuel Lomas <jlomas@mercadona.es>
2024-04-29 10:06:01 +0200

    fix: Updated ListNode class and main class method

---
+++ src/main/java/org/tfoc/ListNode.java
@@ -0,0 +1,16 @@
+package org.tfoc;
+
+/**
+ * Definition for singly-linked list.
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+
+}
--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,16 +1,13 @@
 package org.tfoc;
 
-import java.util.Collections;
-import java.util.List;
-
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
-    public List<String> letterCombinations(String digits) {
+    public boolean hasCycle(ListNode head) {
 
-        return Collections.emptyList();
+        return false;
     }
 
 }
